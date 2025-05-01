62e3d1d5beab30c13e7fb74d08bed5e769b8f35c
Jack Watters <johnwatt@usc.edu>
2025-05-01 01:37:19 -0700

    Enabling CORS

--- packages/core/src/util/index.ts
+++ packages/core/src/util/index.ts
@@ -21,6 +21,10 @@ export namespace Util {
 			return {
 				body,
 				statusCode,
+				headers: {
+					"Access-Control-Allow-Origin": "*",
+					"Access-Control-Allow-Credentials": true,
+				},
 			};
 		};
 	}
