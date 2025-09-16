0f5fbcdec3420064e0fc0122762b85139df5d55a
jackwatters45 <jack.watters@me.com>
2025-09-16 14:36:10 +1000

    fix: tailwind version etc

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,4 +1,3 @@
-import tailwindcss from '@tailwindcss/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -6,11 +5,9 @@ import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
   plugins: [
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    tailwindcss(),
     tanstackStart({
       customViteReactPlugin: true,
     }),
