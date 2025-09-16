5a1563bc2fa67dda7db1960ff8d6c17d9b05b2bb
Jack Watters <jack.watters@me.com>
2025-09-16 14:19:51 +1000

    chore: remove web boilerplate (#35)

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,20 +1,21 @@
+import tailwindcss from '@tailwindcss/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import react from '@vitejs/plugin-react';
+import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
+const config = defineConfig({
   plugins: [
-    tsConfigPaths({
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    tanstackStart(),
-    react(),
+    tailwindcss(),
+    tanstackStart({
+      customViteReactPlugin: true,
+    }),
+    viteReact(),
   ],
-  css: {
-    postcss: './postcss.config.mjs',
-  },
 });
+
+export default config;
