6b1257602067a6c5b4cf34c0fd291b8b9a6dfe99
Jack Watters <jack.watters@me.com>
2025-09-27 22:05:06 +1000

    chore: remove dashboard from scratchpad (#129)

--- scratchpad/src/dashboard/src/components/ui/data-table/TanstackTable.d.ts
+++
@@ -1,8 +0,0 @@
-import '@tanstack/react-table';
-
-declare module '@tanstack/react-table' {
-  interface ColumnMeta<_TData extends RowData, _TValue> {
-    className?: string;
-    displayName: string;
-  }
-}
