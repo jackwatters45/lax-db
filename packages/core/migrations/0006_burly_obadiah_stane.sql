aa25b751bb0a5638217820f322a08cf9596a9ba9
jackwatters45 <jack.watters@me.com>
2025-09-30 15:57:20 +1000

    chore(db): clean up migrations

--- packages/core/migrations/0006_burly_obadiah_stane.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "player" DROP CONSTRAINT "player_user_id_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "player" ALTER COLUMN "user_id" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "player" ADD CONSTRAINT "player_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE set null ON UPDATE no action;
