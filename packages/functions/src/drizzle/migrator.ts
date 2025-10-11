f523a8797c1c92bddfd9e0c4d304344c7dd50990
jackwatters45 <jack.watters@me.com>
2025-10-11 19:19:41 +1100

    refactor: migrator fn

--- packages/functions/src/drizzle/migrator.ts
+++ packages/functions/src/drizzle/migrator.ts
@@ -1,8 +1,32 @@
-import { db } from '@lax-db/core/drizzle/index';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import { Effect, Layer, ManagedRuntime } from 'effect';
+import { Pool } from 'pg';
+import { Resource } from 'sst';
+
+const MainLayer = Layer.empty;
+
+const MigratorRuntime = ManagedRuntime.make(MainLayer);
 
 export const handler = async () => {
-  await migrate(db, {
-    migrationsFolder: 'packages/core/migrations',
-  });
+  MigratorRuntime.runPromise(
+    Effect.gen(function* () {
+      const client = new Pool({
+        ssl: true,
+        host: Resource.Database.host,
+        port: Resource.Database.port,
+        user: Resource.Database.username,
+        password: Resource.Database.password,
+        database: Resource.Database.database,
+      });
+
+      const db = drizzle(client);
+
+      yield* Effect.promise(() =>
+        migrate(db, {
+          migrationsFolder: 'packages/core/migrations',
+        }),
+      );
+    }),
+  );
 };
