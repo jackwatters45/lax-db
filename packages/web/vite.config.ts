c0bd92fac8f346a989a940a8c5d9956eaffbfb15
jackwatters45 <jack.watters@me.com>
2025-10-17 23:55:44 +1100

    feat: nitro config for ts start deploy

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,12 +1,17 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
   plugins: [
     viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
-    // tailwindcss(),
+    nitro({
+      config: {
+        preset: 'aws-lambda',
+      },
+    }),
     tanstackStart(),
     viteReact(),
   ],
